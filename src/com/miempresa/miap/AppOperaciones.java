6c681d0fdb91aeb1cd85ff304bddd0e1e8829420
jose orozco <orozco.jose.7378@eam.edu.co>
2024-10-25 11:25:18 -0500

    ADD AreaCuadrado AreaTriangulo

---
+++ src/com/miempresa/miap/AppOperaciones.java
@@ -0,0 +1,12 @@
+package com.miempresa.miap;
+
+public class AppOperaciones {
+
+		public static double calcularAreaCuadrado(double a, double b) {
+			return a * b;
+		}
+		
+		public static double calcularAreaTriangulo(double altura, double base) {
+			return (altura*base)/2;
+		}
+}
